50983c80c6041641b6705e293fc3f2a535dbc9f1
sergethompson <serge.thompson@gmail.com>
2014-03-07 16:00:47 -0500

    socket setup

--- public/main.js
+++ public/main.js
@@ -9,5 +9,35 @@ content = document.getElementById('content'),
 name = document.getElementById('name');
 
 
+// Connect to websocket
+socket.on('message', function(data){
+	messages.push(data);
+	var html ='';
+
+
+
+	// loop through messages and append them to out chat container
+	for (var i=0; i<messages.length; i++) {
+		html += '<b>' + (messages[i].username ? messages[i].username : 'Server') + ': </b>';
+		html += messages[i].message + '<br />';
+	}
+	// Append message to chatroom
+	content.innerHTML = html;
+});
+
+
+// Submit our message
+submitButton.onclick = function() {
+	var text = message.value;
+	socket.emit('send', { message: text, username: name.value});
+}
+
+$(document).ready(function(){
+	$('#message').keyup(function(e){
+		if(e.keycode === 13){
+			sendMessage();
+		}
+	});
+});
 
 };
